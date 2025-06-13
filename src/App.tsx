7cb350364b90642b5c3de8452627d0e499dbd36d
Danyu Wan <leahw7@protonmail.com>
2025-06-12 21:27:34 -0400

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import Navbar from "./components/Navbar/Navbar.tsx";
+
+function App() {
+
+  return (
+    <>
+      <Navbar></Navbar>
+    </>
+  )
+}
+
+export default App
