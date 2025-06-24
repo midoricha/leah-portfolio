f351aa47ec0f7b60666c38a7037e1a63a8e50332
Danyu Wan <leahw7@protonmail.com>
2025-06-23 20:22:28 -0400

    work on nav bar, use MUI

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,45 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import Navbar from "./components/Navbar/Navbar.tsx";
+import './App.css';
+import './style/variables.css';
+import Navbar from "./components/Navbar/Navbar";
+import {createTheme, ThemeProvider} from "@mui/material";
 
 function App() {
 
-  return (
-    <>
-      <Navbar></Navbar>
-    </>
-  )
+    return (
+        <ThemeProvider theme={theme}>
+            <Navbar/>
+        </ThemeProvider>
+    )
 }
 
-export default App
+const theme = createTheme({
+    components: {
+        // Name of the component
+        MuiAppBar: {
+            styleOverrides: {
+                // Name of the slot
+                root: {
+                    // Some CSS
+                    backgroundColor: 'var(--brown-medium)',
+                    boxShadow: 'none',
+                },
+            },
+        },
+        MuiTypography: {
+            styleOverrides: {
+                root: {
+                    color: 'var(--cream)',
+                }
+            }
+        },
+        MuiButton: {
+            styleOverrides: {
+                root: {
+                    color: 'var(--cream)',
+                }
+            }
+        }
+    },
+});
+
+export default App;
--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,5 +1,27 @@
+import './Navbar.css';
+import {AppBar, Box, Button, Toolbar, Typography} from '@mui/material';
+
+const navItems = ['Home', 'About Me', 'Projects', 'Resumé', 'Contact'];
+
 function Navbar() {
-    return (<></>)
+    return (
+        <AppBar position="static">
+            <Toolbar className="navbar-toolbar">
+                <Typography variant="h6" className="navbar-logo">
+                    <img src="/logo.png" alt="Logo" width={48}
+                         style={{verticalAlign: 'middle', marginRight: 8}}/>
+
+                </Typography>
+                <Box className="navbar-links">
+                    {navItems.map((item) => (
+                        <Button key={item} className="navbar-button">
+                            {item}
+                        </Button>
+                    ))}
+                </Box>
+            </Toolbar>
+        </AppBar>
+    )
 }
 
 export default Navbar;
